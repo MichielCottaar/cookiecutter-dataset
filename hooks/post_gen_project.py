dc815e499abaf76e423a431725d7f23888e59e82
Michiel Cottaar <MichielCottaar@protonmail.com>
2022-09-06 09:50:52 +0100

    Use rmdir to remove directory

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     if "{{ cookiecutter.include_figures }}" == "n":
         for directory in ('final', 'inkscape', 'notebooks'):
             remove_file(os.path.join(directory, 'README.md'))
-            remove_file(os.path.join(directory))
+            os.rmdir(os.path.join(PROJECT_DIRECTORY, directory))
         remove_file('pipe/convert.sh')
         remove_file('pipe/generate.sh')
         remove_file('pipe/to_vault.sh')
